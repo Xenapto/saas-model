5bf2099b587b29ba7adcd53a21160ecf9f645535
Dominic Sayers <dominic@sayers.cc>
2015-03-24 17:52:22 +0000

    Add Setting model

--- app/controllers/model_controller.rb
+++ app/controllers/model_controller.rb
@@ -2,5 +2,7 @@ class ModelController < ApplicationController
   def index
     @features = Feature.all
     @segments = Segment.all
+    @segment_features = SegmentFeature.all
+    @settings = Setting.all
   end
 end
